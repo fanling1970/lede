39dbf54871d9560cde8ffd5ef85e04858ce9b7e5
fanling1970 <zghqidai2008@sina.com>
2026-07-09 05:24:28 +0800

    Update diy-part1.sh

--- libwrt/diy-part1.sh
+++ libwrt/diy-part1.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-# 清理旧重复软件源
+# 清理重复源
 sed -i '/src-git kenzo/d' feeds.conf.default
 sed -i '/src-git small/d' feeds.conf.default
 sed -i '/src-git istore/d' feeds.conf.default
 sed -i '/src-git nas /d' feeds.conf.default
 sed -i '/src-git nas_luci/d' feeds.conf.default
 
-# 添加kenzo、small源
+# 添加插件源
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-# 空行 + istore 商店源
 sed -i '$a \\nsrc-git istore https://github.com/linkease/istore;main' feeds.conf.default
-
-# 空行 + nas相关源
 sed -i '$a \\nsrc-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
 sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 
-# 更新安装feeds
+# 打印源调试，方便看是否写入成功
+cat feeds.conf.default
+
+# 必须完整执行更新+安装
 ./scripts/feeds update -a
 ./scripts/feeds install -a
