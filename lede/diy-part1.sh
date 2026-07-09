beb819293e47be837f4efbb04650752ca8b26ead
fanling1970 <zghqidai2008@sina.com>
2026-07-09 11:54:14 +0800

    Update echo message for LED plugin addition

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -2,4 +2,4 @@
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
-echo "✅ [DIY-P1] 已添加 SSR 插件源"
+echo "✅ [DIY-P1] 已添加 LED 插件源"
