403a82ed8a986d1df547f96bd79064dbe9f7c126
fanling1970 <zghqidai2008@sina.com>
2026-07-09 11:54:39 +0800

    Update diy-part1.sh

--- libwrt/diy-part1.sh
+++ libwrt/diy-part1.sh
@@ -1,30 +1,5 @@
+# 添加京东云 AX6600 LED 控制插件
+git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
+chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
-#!/bin/bash
-# 提前删除官方自带openclash，避免feeds优先加载原版冲突
-rm -rf feeds/luci/applications/luci-app-openclash
-# 提前删除官方passwall，避免优先级冲突
-rm -rf feeds/luci/applications/luci-app-passwall
-
-# 清理旧重复软件源行
-sed -i '/src-git kenzo/d' feeds.conf.default
-sed -i '/src-git small/d' feeds.conf.default
-sed -i '/src-git istore/d' feeds.conf.default
-sed -i '/src-git nas /d' feeds.conf.default
-sed -i '/src-git nas_luci/d' feeds.conf.default
-
-# 追加各类插件源
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a \\nsrc-git istore https://github.com/linkease/istore;main' feeds.conf.default
-sed -i '$a \\nsrc-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
-sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
-
-# 打印源调试，查看是否写入成功
-cat feeds.conf.default
-
-# 清理旧残缺仓库缓存，强制完整拉取
-rm -rf feeds/small feeds/kenzo
-
-# 更新并安装全部feeds
-./scripts/feeds update -a
-./scripts/feeds install -a
+echo "✅ [DIY-P1] 已添加 LED 插件源"
