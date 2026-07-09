96ec84f084529c4ec110c09ace11d98b0dfed678
fanling1970 <zghqidai2008@sina.com>
2026-07-09 08:28:22 +0800

    Update diy-part1.sh

--- libwrt/diy-part1.sh
+++ libwrt/diy-part1.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+# 提前删除官方openclash，避免优先级冲突
+rm -rf feeds/luci/applications/luci-app-openclash
+# 提前删除官方passwall，避免优先级冲突
+rm -rf feeds/luci/applications/luci-app-passwall
+
 # 清理重复源
 sed -i '/src-git kenzo/d' feeds.conf.default
 sed -i '/src-git small/d' feeds.conf.default
@@ -6,16 +10,16 @@ sed -i '/src-git istore/d' feeds.conf.default
 sed -i '/src-git nas /d' feeds.conf.default
 sed -i '/src-git nas_luci/d' feeds.conf.default
 
-# 添加插件源
+# 写入软件源
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a \\nsrc-git istore https://github.com/linkease/istore;main' feeds.conf.default
 sed -i '$a \\nsrc-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
 sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 
-# 打印源调试，方便看是否写入成功
 cat feeds.conf.default
 
-# 必须完整执行更新+安装
+# 删除残缺的旧small目录，强制重新克隆完整仓库
+rm -rf feeds/small
 ./scripts/feeds update -a
 ./scripts/feeds install -a
