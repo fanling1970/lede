6b742e75697efe63d8987490b8b1c2437db22750
fanling1970 <zghqidai2008@sina.com>
2026-07-13 09:15:37 +0800

    Update diy-part2.sh

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -1,27 +1,59 @@
 #!/bin/bash
-# diy-part2.sh - 在 feeds update 之前执行
+# diy-part2.sh
 
 # ========== 基础设置修改 ==========
-# 修改 device 设备名称
+# 修改 device 设备名称（保留）
 sed -i "s/hostname='.*'/hostname='LEDE'/g" package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='LEDE'/g" package/base-files/luci2/bin/config_generate
 
-# 加入作者信息
+# 加入作者信息（保留）
 sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt-$(date +%Y%m%d)'/g" package/lean/default-settings/files/zzz-default-settings   
 sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By J.Y'/g" package/lean/default-settings/files/zzz-default-settings
 
 sed -i "2iuci set istore.istore.channel='OpenWrt'" package/lean/default-settings/files/zzz-default-settings
 sed -i "3iuci commit istore" package/lean/default-settings/files/zzz-default-settings
 
-# 默认网关 ip 地址修改
+# 默认网关 ip 地址修改（保留）
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/luci2/bin/config_generate
 
-# 修改 wifi 无线名称
-sed -i "s/LEDE/JDC_AX6600/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改 wifi 无线名称（删除或注释掉这行）
+# sed -i "s/LEDE/JDC_AX6600/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 清除默认密码 password
+# 清除默认密码 password（保留）
 sed -i '/V4UetPzk$CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+
+# ========== 添加无线网络配置 ==========
+cat >> package/lean/default-settings/files/zzz-default-settings << 'EOF'
+
+# 设置无线网络 - radio0 (5G)
+uci set wireless.radio0.channel='149'
+uci set wireless.radio0.band='5g'
+uci set wireless.radio0.htmode='HE80'
+uci set wireless.@wifi-iface[0].ssid='JDC_AX6600_5G'
+uci set wireless.@wifi-iface[0].key='BUZHIDAOWA'
+uci set wireless.@wifi-iface[0].encryption='psk2'
+
+# 设置无线网络 - radio1 (2.4G)
+uci set wireless.radio1.channel='6'
+uci set wireless.radio1.band='2g'
+uci set wireless.radio1.htmode='HT40'
+uci set wireless.@wifi-iface[1].ssid='JDC_AX6600_2.4G'
+uci set wireless.@wifi-iface[1].key='BUZHIDAOWA'
+uci set wireless.@wifi-iface[1].encryption='psk2'
+
+# 设置无线网络 - radio2 (5G2)
+uci set wireless.radio2.channel='44'
+uci set wireless.radio2.band='5g'
+uci set wireless.radio2.htmode='HE160'
+uci set wireless.@wifi-iface[2].ssid='JDC_AX6600_5G2'
+uci set wireless.@wifi-iface[2].key='BUZHIDAOWA'
+uci set wireless.@wifi-iface[2].encryption='psk2'
+
+# 提交无线配置更改
+uci commit wireless
+EOF
+
 
 # ========== 删除冲突包（必须在 feeds install 之前）==========
 # 删除源码自带的 openclash，避免和 kenzok8/small 里的冲突
